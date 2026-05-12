e6efb03c54aa72171e0f25c0803181e8985750c9
Anton Hvrylchyk <anton.g.2.9x@gmail.com>
2026-05-12 21:01:34 +0300

    left sidebar

--- src/components/sidebar-left/sidebar-left.jsx
+++ src/components/sidebar-left/sidebar-left.jsx
@@ -1,22 +1,55 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styles from "./sidebar-left.module.css";
 
 export function SidebarLeft() {
+  // Список ресурсів, які ми реально використовували
+  const techStack = [
+    { name: 'React', url: 'https://react.dev', icon: '⚛️' },
+    { name: 'Firebase', url: 'https://firebase.google.com', icon: '🔥' },
+    { name: 'React Router', url: 'https://reactrouter.com', icon: '🛣️' },
+    { name: 'Firestore', url: 'https://firebase.google.com/docs/firestore', icon: '📦' },
+    { name: 'CSS Modules', url: 'https://github.com/css-modules/css-modules', icon: '🎨' }
+  ];
+
   return (
     <nav className={styles.sidebar}>
-      <div className={styles.menuGroup}>
-        <Link to="/" className={styles.navLink}>🏠 Головна</Link>
-        <Link to="/games" className={styles.navLink}>🎮 Ігри</Link>
-        <Link to="/hardware" className={styles.navLink}>🖥️ Залізо</Link>
+      {/* НОВИЙ БЛОК: Технології (замість навігації) */}
+      <div className={styles.group}>
+        <h4 className={styles.groupTitle}>ТЕХНОЛОГІЇ</h4>
+        {techStack.map((tech) => (
+          <a 
+            key={tech.name}
+            href={tech.url} 
+            target="_blank" 
+            rel="noreferrer" 
+            className={styles.link}
+          >
+            <span className={styles.icon}>{tech.icon}</span> {tech.name}
+          </a>
+        ))}
       </div>
 
-      <div className={styles.divider}></div>
+      <hr className={styles.divider} />
 
-      <div className={styles.socialGroup}>
-        <h4 className={styles.groupTitle}>Спільнота</h4>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1" target="_blank" rel="noreferrer" className={styles.socialLink}>Discord</a>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1" target="_blank" rel="noreferrer" className={styles.socialLink}>Telegram</a>
+      {/* Спільнота залишається на місці */}
+      <div className={styles.group}>
+        <h4 className={styles.groupTitle}>СПІЛЬНОТА</h4>
+        <a 
+          href="https://discord.com" 
+          target="_blank" 
+          rel="noreferrer" 
+          className={styles.link}
+        >
+          Discord
+        </a>
+        <a 
+          href="https://t.me" 
+          target="_blank" 
+          rel="noreferrer" 
+          className={styles.link}
+        >
+          Telegram
+        </a>
       </div>
     </nav>
   );
